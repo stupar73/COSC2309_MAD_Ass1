9b2ab9f3a0e598247c2e3beb20b47f37ae74b531
Stuart Parker <stu_par73@hotmail.com>
2016-08-17 21:51:30 +1000

    Add beginning of SimpleEvent class

---
+++ app/src/main/java/s3390317/mad/ass1/model/SimpleEvent.java
@@ -0,0 +1,19 @@
+package s3390317.mad.ass1.model;
+
+import java.util.Calendar;
+
+/**
+ * Created by Stuart on 17/08/2016.
+ */
+public class SimpleEvent
+{
+    private String id;
+    private String title;
+    private Calendar start;
+    private Calendar end;
+    private String venue;
+    private String location;
+    private String note;
+    // private List<People?> attendees;
+
+}
