da57f01d09894a279a37a127af9b213facabff1d
Stuart Parker <stu_par73@hotmail.com>
2016-08-18 19:30:37 +1000

    Rename Event classes, start implementing EventModel

---
+++ app/src/main/java/s3390317/mad/ass1/controller/EditEventController.java
@@ -0,0 +1,15 @@
+package s3390317.mad.ass1.controller;
+
+import android.view.View;
+
+/**
+ * Created by stuart on 18/08/16.
+ */
+public class EditEventController implements View.OnClickListener
+{
+    @Override
+    public void onClick(View view)
+    {
+
+    }
+}
---
+++ app/src/main/java/s3390317/mad/ass1/view/EditEventActivity.java
@@ -0,0 +1,17 @@
+package s3390317.mad.ass1.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import s3390317.mad.ass1.R;
+
+public class EditEventActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_event);
+    }
+}
